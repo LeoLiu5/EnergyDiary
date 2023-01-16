4df838f55fea602d04da4fa9d461b337b362e450
Leo Liu <liuxiaochen11@gmail.com>
2023-01-16 12:32:32 +0000

    Update Peter2.dart

--- EnergyDiary/lib/Peter2.dart
+++ EnergyDiary/lib/Peter2.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+const String _name = "hekaiyou";
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                margin: const EdgeInsets.only(right: 16.0),
+                child: new CircleAvatar(child: new Text(_name[0])),
+              ),
+              new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(_name),
+                    new Container(
+                      margin: const EdgeInsets.only(top: 5.0),
+                      child: new Text(text),
+                    )
+                  ])
+            ]));
+  }
+}
+
 class PeterC extends StatefulWidget {
   const PeterC({Key? key}) : super(key: key);
 
@@ -30,9 +59,17 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+
   void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
   }
 
   Widget _buildTextComposer() {
@@ -59,6 +96,22 @@ class ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(body: _buildTextComposer());
+    return new Scaffold(
+        body: new Column(children: <Widget>[
+      new Flexible(
+          child: new ListView.builder(
+        padding: new EdgeInsets.all(8.0),
+        reverse: true,
+        itemBuilder: (_, int index) => _messages[index],
+        itemCount: _messages.length,
+      )),
+      new Divider(height: 1.0),
+      new Container(
+        decoration: new BoxDecoration(
+          color: Theme.of(context).cardColor,
+        ),
+        child: _buildTextComposer(),
+      )
+    ]));
   }
 }
